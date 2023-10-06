4daf8eaf9ce08c5b40c4bded31f1ba03fd6c0d86
alaa damen <alae.damen1@gmail.com>
2023-10-05 20:03:28 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /** 
- * main -A program that prints a line using puts 
- * return : 0 (success)
+ * main - a program that prints a line using puts 
+ * return : always 0 (success)
  */
 int main(void)
 {
