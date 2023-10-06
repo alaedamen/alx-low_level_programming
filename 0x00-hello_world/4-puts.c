5b9acd4a07c5976d1812eb0073b2b581f2b9c5b8
alaa damen <alae.damen1@gmail.com>
2023-10-05 19:13:16 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,10 @@
 /** 
  * main -A program that prints a line using puts 
  * return : 0 (success)
-*/
+ */
 int main(void)
 {
-        puts("Programming is like building a multilingual puzzle\n");
+        puts("\”Programming is like building a multilingual puzzle\n");
         return (0);
 }
+
