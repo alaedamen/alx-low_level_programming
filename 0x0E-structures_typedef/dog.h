634358f38c9771123bdae489b7bb406e105226cd
alaedamen <alae.damen1@gmail.com>
2023-11-06 17:18:48 -0800

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,37 +1,19 @@
-#ifndef DOG
-#define DOG
+#ifndef _DOG_H
+#define _DOG_H
 
-
-   /**
-    * struct dog - dog class
-    *
-    * @name: Name of the dog
-    *
-    * @age: Age of the dog
-    *
-    * @owner: Owner of the dog
-    */
-
-
+/**
+ * struct dog - a struct that create a dog info
+ *
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * description: first struct with alx
+ */
 struct dog
 {
-	char *name;
-	float age;
-	char *owner;
+char *name;
+float age;
+char *owner;
 };
-
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-
-
-typedef struct dog dog_t;
-
-
-dog_t *new_dog(char *name, float age, char *owner);
-
-
-void free_dog(dog_t *d);
-
-
 #endif
